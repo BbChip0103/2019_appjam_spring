fb78fb337eaa5b2675491c01eb8eb968ca4c5d23
BbChip0103 <bbchip13@gmail.com>
2019-04-21 03:50:17 +0900

    Upload file

--- raspberry_pi/scan.py
+++ raspberry_pi/scan.py
@@ -1,15 +1,5 @@
 from bluepy.btle import Scanner, DefaultDelegate
 
-class ScanDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
-
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            print("Discovered device", dev.addr)
-        elif isNewData:
-            print("Received new data from", dev.addr)
-
 scanner = Scanner()
 devices = scanner.scan(1.0)
 
